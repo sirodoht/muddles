5820e9bb3573133fe63076f4c4db77292f76e795
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-07-06 23:17:34 +0300

    add github auth routes

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const indexCtrl = require('../controllers/index.ctrl');
 const userCtrl = require('../controllers/user.ctrl');
 
+router.get('/', indexCtrl.getIndex);
+router.get('/login', userCtrl.getLogin);
+
+router.get('/auth/github', passport.authenticate('github'));
+
+router.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    console.log('success');
+    res.redirect('/');
+  });
+
 router.post('/register', userCtrl.register);
 router.post('/login', userCtrl.login);
 
-router.get('/', indexCtrl.getIndex);
 
 router.get('/register', userCtrl.getRegister);
-router.get('/login', userCtrl.getLogin);
 
 router.get('/:user', userCtrl.getUser);
 
