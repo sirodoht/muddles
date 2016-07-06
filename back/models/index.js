084159069f5e761e674c47a1767c89332efd9eaa
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-07-06 23:53:16 +0300

    fix postgres credentials

--- back/models/index.js
+++ back/models/index.js
@@ -4,7 +4,7 @@ const config = require('config');
 const Sequelize = require('sequelize');
 
 const configPostgres = config.get('postgres');
-const sequelize = new Sequelize('postgres://postgres:8d38d31570aa40e9795f3df17a75b5f0@dokku-postgres-muddles:5432/muddles');
+const sequelize = new Sequelize(configPostgres.url);
 // const sequelize = new Sequelize(configPostgres.database, configPostgres.uername, configPostgres.password, {
 //   host: configPostgres.host,
 //   dialect: 'postgres',
