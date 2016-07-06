18b85ce02b8ba43f46a765f97e940dd51a932232
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-07-06 23:39:33 +0300

    hardcode uri string

--- back/models/index.js
+++ back/models/index.js
@@ -4,10 +4,11 @@ const config = require('config');
 const Sequelize = require('sequelize');
 
 const configPostgres = config.get('postgres');
-const sequelize = new Sequelize(configPostgres.database, configPostgres.uername, configPostgres.password, {
-  host: configPostgres.host,
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize('postgres://postgres:8d38d31570aa40e9795f3df17a75b5f0@dokku-postgres-muddles:5432/muddles');
+// const sequelize = new Sequelize(configPostgres.database, configPostgres.uername, configPostgres.password, {
+//   host: configPostgres.host,
+//   dialect: 'postgres',
+// });
 
 const user = sequelize.import(path.join(__dirname, 'user.model.js'));
 
