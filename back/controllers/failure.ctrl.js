0a06e68d9ef6fc59a5fad544d6c891d8cae40be5
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-07-09 23:39:48 +0300

    add failure list method

--- back/controllers/failure.ctrl.js
+++ back/controllers/failure.ctrl.js
@@ -2,10 +2,16 @@ const models = require('../models');
 
 const failureCtrl = module.exports = {};
 
-failureCtrl.register = function (req, res) {
-  console.log('req.body:', req.body);
+failureCtrl.new = function (req, res) {
   models.Failure.create(req.body)
     .then(function () {
       res.redirect('/list');
+    });
+};
+
+failureCtrl.list = function (req, res) {
+  models.Failure.findAll()
+    .then(function (failures) {
+      res.json(failures);
     });
 };
