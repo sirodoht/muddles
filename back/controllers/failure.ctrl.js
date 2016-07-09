dff3743bf94f44df0ea24a970a253c4404743cd8
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-07-10 02:44:04 +0300

    add read failure method

--- back/controllers/failure.ctrl.js
+++ back/controllers/failure.ctrl.js
@@ -12,6 +12,25 @@ failureCtrl.new = function (req, res) {
 failureCtrl.list = function (req, res) {
   models.Failure.findAll()
     .then(function (failures) {
-      res.json(failures);
+      res.render('failures', {
+        brand: 'Muddles',
+        title: 'readAll',
+        subtitle: 'The list of the things that suck.',
+        failures,
+      });
+    });
+};
+
+failureCtrl.read = function (req, res) {
+  models.Failure.findOne({
+    where: { id: req.params.failureId }
+  })
+    .then(function (failure) {
+      res.render('failure', {
+        brand: 'Muddles',
+        title: 'readOne',
+        subtitle: 'The one of the things that suck.',
+        failure,
+      });
     });
 };
