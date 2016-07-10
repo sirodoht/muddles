a783022185e4588e1696736678b486292fff879d
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-07-11 00:45:19 +0300

    comment in auth midd

--- back/app.js
+++ back/app.js
@@ -87,24 +87,25 @@ passport.deserializeUser(function (githubId, done) {
 
 app.use(express.static(path.join(__dirname, '../front/static')));
 
-// app.use(function (req, res, next) {
-//   console.log('req.user', req.user);
-//   if (req.isAuthenticated()) {
-//     return models.User.findOne({
-//       where: {
-//         githubId: req.user.id,
-//       }
-//     })
-//       .then(function (user) {
-//         res.locals.auth = true;
-//         res.locals.avatar = user.avatar;
-//         res.locals.profile = user.profile;
-//         next();
-//       });
-//   } else {
-//     next();
-//   }
-// });
+app.use(function (req, res, next) {
+  console.log('req.user', req.user);
+  if (req.isAuthenticated()) {
+    return models.User.findOne({
+      where: {
+        githubId: req.user.id,
+      }
+    })
+      .then(function (user) {
+        console.log('db user', user);
+        res.locals.auth = true;
+        res.locals.avatar = user.avatar;
+        res.locals.profile = user.profile;
+        next();
+      });
+  } else {
+    next();
+  }
+});
 
 app.use('/', routes);
 
