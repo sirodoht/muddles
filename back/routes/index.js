c2903ba34943ae6d5334ce3022f6b05b0e0de183
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-07-10 19:27:01 +0300

    try reload

--- back/routes/index.js
+++ back/routes/index.js
@@ -12,11 +12,12 @@ router.get('/login', userCtrl.getLogin);
 router.get('/auth/github', passport.authenticate('github'));
 
 router.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
+  passport.authenticate('github', { failureRedirect: '/' }),
   function(req, res) {
     // Successful authentication, redirect home.
     console.log('req.isAuthenticated():', req.isAuthenticated());
     console.log('req.session.save:', req.session.save);
+    req.session.reload();
     req.session.save();
     req.session.user = req.user;
     res.redirect('/');
